c11009e071119dece4074811a0e49fc9d900a6b1
Dhui-Park <hasong925@gmail.com>
2025-06-04 16:21:53 +0900

    Price Chart part one

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,69 @@
-function Chart() {
-    return <h1>Chart</h1>;
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+interface ChartProps {
+    coinId: string;
+}
+
+function Chart( { coinId }: ChartProps ) {
+    const {isLoading, data} = useQuery<IHistorical[]>({
+        queryKey: ["ohlcv", coinId],
+        queryFn: () => fetchCoinHistory(coinId)
+    });
+    return (
+        <div>
+            {isLoading ? "Loading chart..." : <ApexChart type="line" series={[
+                {
+                    name: "Chart",
+                    data: data?.map(price => price.close) ?? []
+                }
+            ]} options={{
+                theme: {
+                    mode: "dark",
+                }, 
+                chart: {
+                    height: 300,
+                    width: 500,
+                    toolbar: {
+                        show: false,
+                    },
+                },
+                grid: {
+                    show: false,
+                },
+                stroke: {
+                    curve: "smooth",
+                    width: 3,
+                },
+                yaxis: {
+                    show: false,
+                },
+                xaxis: {
+                    axisTicks: {
+                        show: false,
+                    },
+                    axisBorder: {
+                        show: false,
+                    },
+                    labels: {
+                        show: false,
+                    }
+                },
+            }} />}
+        </div>
+    );
 }
 
 export default Chart;
